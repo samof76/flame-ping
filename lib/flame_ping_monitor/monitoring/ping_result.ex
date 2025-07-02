d9782e3e89972474fcf4bc1c9b0d32b5f0544d8f
Fly Dev <dev@fly.io>
2025-07-02 06:15:15 +0000

    add region field to PingResult schema

--- lib/flame_ping_monitor/monitoring/ping_result.ex
+++ lib/flame_ping_monitor/monitoring/ping_result.ex
@@ -7,6 +7,7 @@ defmodule FlamePingMonitor.Monitoring.PingResult do
     field :response_time, :integer
     field :pinged_at, :naive_datetime
     field :error_message, :string
+    field :region, :string
     belongs_to :domain, FlamePingMonitor.Monitoring.Domain
 
     timestamps()
@@ -15,9 +16,10 @@ defmodule FlamePingMonitor.Monitoring.PingResult do
   @doc false
   def changeset(ping_result, attrs) do
     ping_result
-    |> cast(attrs, [:status, :response_time, :pinged_at, :error_message, :domain_id])
-    |> validate_required([:status, :pinged_at, :domain_id])
+    |> cast(attrs, [:status, :response_time, :pinged_at, :error_message, :domain_id, :region])
+    |> validate_required([:status, :pinged_at, :domain_id, :region])
     |> validate_inclusion(:status, ["online", "offline", "timeout"])
+    |> validate_inclusion(:region, ["na", "eu", "as", "sa", "oc"])
     |> foreign_key_constraint(:domain_id)
   end
 end
